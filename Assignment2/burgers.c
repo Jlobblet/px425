247aef478a890c3edc25e3b3119ee5fa34906da2
Jlobblet <jlobblet-github@gmx.co.uk>
2021-11-07 22:10:09 +0000

    Assignment 2 initial commit

---
+++ Assignment2/burgers.c
@@ -0,0 +1,249 @@
+/*==========================================================//
+//  ANSI-C code (un optimised) for PX425 assignment 2 2021  //
+//  Evolves a function u in 2D via finite differences       //
+//  of the 2D Burgers Equation (simplified)                 //
+//                                                          //
+//  d u          du   du           d^2       d^2            //
+//  ----- = -u ( -- + -- ) + nu ( ---- u  +  ---- u )       //
+//  d t          dx   dy          dx^2       dy^2           //
+//                                                          //
+//  Based originally on code created by D. Quigley          //
+//  Adapted by N. Hine                                      //
+//==========================================================*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "makePNG.h"     /* For visualisation */
+#include "mt19937ar.h"   /* Random number generator */
+
+/* Function prototypes for memory management routines */
+void allocate2d(double ***a,int num_rows,int num_cols);
+void free2d(double ***a,int num_rows);
+
+int main () {
+
+  /* Function u on new and current grid */
+  double **u_new, **u;
+
+  /* Approximate Laplacian */
+  double Lapl, grad;
+
+  /* Number of grid points */
+  int Nx = 256;
+  int Ny = 256;
+
+  /* Loop counters */
+  int ix,iy,istep;
+
+  /* Filename to which the grid is drawn */
+  int  isnap=0;
+  char filename[25];
+
+  /*--------------*/
+  /* Initial time */
+  /*--------------*/
+  clock_t t1 = clock();
+
+  /*------------------------------------*/
+  /* Initialise random number generator */
+  /*------------------------------------*/
+  unsigned long seed = 120549784972;
+  init_genrand(seed);
+
+  /*--------------------------*/
+  /* Set grid spacing         */
+  /*--------------------------*/
+  double dx = 1.0;
+  double dy = 1.0;
+
+  /*-----------------------------*/
+  /* Set timestep and K          */
+  /*-----------------------------*/
+  double dt = 0.001;
+  double nu = 5.0;
+
+  /*--------------------------*/
+  /* Number of steps to run   */
+  /*--------------------------*/
+  int nstep = 10000;
+
+  /*--------------------------------------*/
+  /* Allocate memory for a bunch of stuff */
+  /*--------------------------------------*/
+  allocate2d(&u,Nx,Ny);
+  allocate2d(&u_new,Nx,Ny);
+
+  /*--------------------------------*/
+  /* Initialise with random numbers */
+  /*--------------------------------*/
+  for(ix=0;ix<Nx;ix++) {
+    for(iy=0;iy<Ny;iy++) {
+      u[ix][iy] = 2.0*genrand() - 1.0;
+    }
+  }
+      
+  /*------------------------------------*/
+  /* Write an image of the initial grid */
+  /*------------------------------------*/
+  int stepincr = 10; 
+  sprintf(filename,"snapshot%08d.png",isnap); 
+  writePNG(filename,u,Nx,Ny); 
+  isnap++;
+
+  /* setup time */
+  clock_t t2 = clock();
+  printf("Setup time                    : %15.6f seconds\n",(double)(t2-t1)/(double)CLOCKS_PER_SEC);
+  t1 = t2;
+
+  /*===============================*/
+  /* BEGIN SECTION TO BE OPTIMISED */
+  /*===============================*/
+ 
+  /*------------------------------------------*/
+  /* Loop over the number of output timesteps */
+  /*------------------------------------------*/
+  for (istep=1;istep<nstep;istep++) {
+    
+    /*-----------------------*/
+    /* Loop over grid points */
+    /*-----------------------*/
+    for(iy=0;iy<Ny;iy++) {
+      for(ix=0;ix<Nx;ix++) {
+
+	Lapl = 0.0; /* Initialise finite-difference Laplacian */
+        grad = 0.0; /* Initialise gradient */
+	
+        /* compute d2u/dx2, accounting for special cases near boundaries */
+        if (ix==0) {
+	  Lapl += -2.0*u[ix][iy]/pow(dx,2.0) + u[ix+1][iy]/pow(dx,2.0) + u[Nx-1][iy]/pow(dx,2.0);  /* left */
+          grad += (u[ix+1][iy] - u[Nx-1][iy])/dx/2.0;
+        } else if (ix==Nx-1) {
+	  Lapl += -2.0*u[ix][iy]/pow(dx,2.0) + u[0][iy]/pow(dx,2.0) + u[ix-1][iy]/pow(dx,2.0);     /* right */
+          grad += (u[0][iy] - u[ix-1][iy])/dx/2.0;
+        } else {
+	  Lapl += -2.0*u[ix][iy]/pow(dx,2.0) + u[ix+1][iy]/pow(dx,2.0) + u[ix-1][iy]/pow(dx,2.0);
+          grad += (u[ix+1][iy] - u[ix-1][iy])/dx/2.0;
+        }
+        
+        /* compute d2u/dy2, accounting for special cases near boundaries */
+        if (iy==0) {
+	  Lapl += -2.0*u[ix][iy]/pow(dy,2.0) + u[ix][iy+1]/pow(dy,2.0) + u[ix][Ny-1]/pow(dy,2.0);  /* bottom */
+          grad += (u[ix][iy+1] - u[ix][Ny-1])/dy/2.0;
+        } else if (iy==Ny-1) {
+	  Lapl += -2.0*u[ix][iy]/pow(dy,2.0) + u[ix][0]/pow(dy,2.0) + u[ix][iy-1]/pow(dy,2.0);     /* top */ 
+          grad += (u[ix][0] - u[ix][iy-1])/dy/2.0;
+        } else {
+	  Lapl += -2.0*u[ix][iy]/pow(dy,2.0) + u[ix][iy+1]/pow(dy,2.0) + u[ix][iy-1]/pow(dy,2.0);  
+          grad += (u[ix][iy+1] - u[ix][iy-1])/dy/2.0;
+        }
+        
+        /* compute new value of u at this grid point */
+        u_new[ix][iy] = u[ix][iy] - dt*u[ix][iy]*grad + dt*nu*Lapl;
+        
+      }
+    }
+        
+    /* Shunt u_new into u */
+    for(iy=0;iy<Ny;iy++) {
+      for(ix=0;ix<Nx;ix++) {
+        u[ix][iy] = u_new[ix][iy];
+      }
+    }
+
+    /*-----------------------------*/
+    /* Snapshots of grid to file   */
+    /*-----------------------------*/
+    if ( istep==isnap)  { 
+        sprintf(filename,"snapshot%08d.png",isnap); 
+        writePNG(filename,u,Nx,Ny); 
+        isnap *= stepincr; 
+    }  
+
+  }
+
+  /*=============================*/
+  /* END SECTION TO BE OPTIMISED */
+  /*=============================*/
+  
+  /* calculation time */
+  t2 = clock();
+  printf("Time taken for %8d steps : %15.6f seconds\n",nstep,(double)(t2-t1)/(double)CLOCKS_PER_SEC);
+    
+  /*----------------------------------*/
+  /* Write an image of the final grid */
+  /*----------------------------------*/
+  sprintf(filename,"snapshot%08d.png",istep); 
+  writePNG(filename,u,Nx,Ny); 
+
+  /*--------------------------------------------*/
+  /* Write final time-evolved solution to file. */
+  /*--------------------------------------------*/
+  FILE *fp = fopen("final_grid.dat","w");
+  if (fp==NULL) printf("Error opening final_grid.dat for output\n");
+
+  for(ix=0;ix<Nx-1;ix++) {
+    for(iy=0;iy<Ny-1;iy++) {
+      /* x and y at the current grid points */
+      double x = dx*(double)ix;
+      double y = dy*(double)iy;
+      fprintf(fp,"%8.4f %8.4f %8.4e\n",x,y,u[ix][iy]);
+    }
+    fprintf(fp,"\n");
+  }
+  fclose(fp);
+
+  /* Release memory */
+  free2d(&u,Nx);
+  free2d(&u_new,Nx);
+  
+  return 0;
+  
+}
+  
+
+
+/*===========================================*/
+/* Auxilliary routines for memory management */ 
+/*===========================================*/
+void allocate2d(double ***a,int Nx,int Ny) {
+
+  double **b_loc; 
+
+  b_loc = (double **)calloc(Nx,sizeof(double *));
+  if (b_loc==NULL) printf("malloc error in allocate2d\n"); 
+
+  int iy;
+  for (iy=0;iy<Nx;iy++) {
+    
+    b_loc[iy] = (double *)calloc(Ny,sizeof(double));
+    if (b_loc[iy]==NULL) printf("malloc error for row %d of %d in allocate2d\n",iy,Nx);
+
+  }
+
+  *a = b_loc;
+
+}
+
+void free2d(double ***a,int Nx) {
+
+  int iy;
+
+  double **b_loc = *a;
+
+  /* Release memory */
+  for (iy=0;iy<Nx;iy++) { 
+    free(b_loc[iy]);
+  }
+  free(b_loc);
+  *a = b_loc;
+
+}
+
+
+
+
+
+
+
+
