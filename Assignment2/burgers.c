b6575be0144b926c35e519a228bb62180551a497
Jlobblet <jlobblet-github@gmx.co.uk>
2021-11-11 16:35:42 +0000

    C99 comment style

--- Assignment2/burgers.c
+++ Assignment2/burgers.c
@@ -1,13 +1,13 @@
-/*  ANSI-C code (un optimised) for PX425 assignment 2 2021
- *  Evolves a function u in 2D via finite differences
- *  of the 2D Burgers Equation (simplified)
- *
- *  d u          du   du           d^2       d^2
- *  ----- = -u ( -- + -- ) + nu ( ---- u  +  ---- u )
- *  d t          dx   dy          dx^2       dy^2
- *
- *  Based originally on code created by D. Quigley
- *  Adapted by N. Hine */
+//  ANSI-C code (un optimised) for PX425 assignment 2 2021
+// Evolves a function u in 2D via finite differences
+//  of the 2D Burgers Equation (simplified)
+//
+//  d u          du   du           d^2       d^2
+//  ----- = -u ( -- + -- ) + nu ( ---- u  +  ---- u )
+//  d t          dx   dy          dx^2       dy^2
+//
+//  Based originally on code created by D. Quigley
+//  Adapted by N. Hine
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -15,7 +15,7 @@
 #include "makePNG.h"
 #include "mt19937ar.h"
 
-/* Function prototypes for memory management routines */
+// Function prototypes for memory management routines
 
 #ifdef __OPTIMIZE__
 void allocate2d(double*** a, int Nx, int Ny) {
@@ -43,7 +43,6 @@ void allocate2d(double*** a, int Nx, int Ny) {
 
 void free2d(double*** a) {
     double** b_loc = *a;
-    /* Release memory */
     free(b_loc[0]);
     free(b_loc);
     *a = NULL;
@@ -76,7 +75,6 @@ void allocate2d(double*** a, int Nx, int Ny) {
 void free2d(double*** a, int Nx) {
     int iy;
     double** b_loc = *a;
-    /* Release memory */
     for (iy = 0; iy < Nx; iy++) {
         free(b_loc[iy]);
     }
@@ -86,31 +84,31 @@ void free2d(double*** a, int Nx) {
 #endif // __OPTIMIZE__
 
 int main() {
-    /* Function u on new and current grid */
+    // Function u on new and current grid
     double** u_new, ** u;
 
-    /* Approximate Laplacian */
+    // Approximate Laplacian
     double Lapl, grad;
 
-    /* Number of grid points */
+    // Number of grid points
     const int Nx = 256;
     const int Ny = 256;
 
-    /* Loop counters */
+    // Loop counters
     int ix, iy, istep;
 
-    /* Filename to which the grid is drawn */
+    // Filename to which the grid is drawn
     int isnap = 0;
     char filename[25];
 
-    /* Initial time */
+    // Initial time
     clock_t t1 = clock();
 
-    /* Initialise random number generator */
+    // Initialise random number generator
     const unsigned long seed = 120549784972;
     init_genrand(seed);
 
-    /* Set grid spacing */
+    // Set grid spacing
     const double dx = 1.0;
     const double dy = 1.0;
     const double dx_sq = dx * dx;
@@ -122,25 +120,26 @@ int main() {
     const double half_inv_dx = inv_dx * 0.5;
     const double half_inv_dy = inv_dy * 0.5;
 
-    /* Set timestep and K */
+    // Set timestep and K
     const double dt = 0.001;
     const double nu = 5.0;
 
-    /* Number of steps to run */
+    // Number of steps to run
     const int nstep = 10000;
 
-    /* Allocate memory for a bunch of stuff */
+    // Allocate memory for u and u_new
     allocate2d(&u, Nx, Ny);
     allocate2d(&u_new, Nx, Ny);
 
-    /* Initialise with random numbers */
+    // Initialise u with random numbers
     for (ix = 0; ix < Nx; ix++) {
         for (iy = 0; iy < Ny; iy++) {
             u[ix][iy] = 2.0 * genrand() - 1.0;
         }
     }
 
-    /* Write an image of the initial grid */
+    // Write an image of the initial grid
+    // This ifdef is to prevent an unused variable warning when NDEBUG is not set.
 #ifdef NDEBUG
     int stepincr = 10;
 #endif // NDEBUG
@@ -148,31 +147,31 @@ int main() {
     writePNG(filename, u, Nx, Ny);
     isnap++;
 
-    /* Setup time */
+    // Setup time
     clock_t t2 = clock();
     printf("Setup time                    : %15.6f seconds\n", (double) (t2 - t1) / (double) CLOCKS_PER_SEC);
     fflush(stdout);
     t1 = t2;
 
-    /* BEGIN SECTION TO BE OPTIMISED */
+    // BEGIN SECTION TO BE OPTIMISED
 
-    /* Loop over the number of output timesteps */
+    // Loop over the number of output timesteps
     for (istep = 1; istep < nstep; istep++) {
 
-        /* Quick diagram to label the upcoming sections:
-         *
-         * CEEEEC
-         * EIIIIE
-         * EIIIIE
-         * EIIIIE
-         * EIIIIE
-         * CEEEEC
-         *
-         * C corner
-         * E edge
-         * I interior */
+        //  Quick diagram to label the upcoming sections:
+        //
+        // CEEEEC
+        // EIIIIE
+        // EIIIIE
+        // EIIIIE
+        // EIIIIE
+        // CEEEEC
+        //
+        // C: corner
+        // E: edge
+        // I: interior
 
-        /* Corners */
+        // Corners
 
         double u00 = u[0][0];
         double u10 = u[1][0];
@@ -185,41 +184,41 @@ int main() {
         double u1E = u[1][Ny - 1];
         double u0e = u[0][Ny - 2];
 
-        /* Bottom left
-         * ix = 0, iy = 0 */
-        Lapl = (-2.0 * u00 + u10 + uE0) * inv_dx_sq /* left */
-               + (-2.0 * u00 + u01 + u0E) * inv_dy_sq; /* bottom*/
-        grad = (u10 - uE0) * half_inv_dx /* left */
-               + (u01 - u0E) * half_inv_dy; /* bottom */
+        // Bottom left
+        // ix = 0, iy = 0
+        Lapl = (-2.0 * u00 + u10 + uE0) * inv_dx_sq // left
+               + (-2.0 * u00 + u01 + u0E) * inv_dy_sq; // bottom
+        grad = (u10 - uE0) * half_inv_dx // left
+               + (u01 - u0E) * half_inv_dy; // bottom
         u_new[0][0] = u00 - dt * u00 * grad + dt * nu * Lapl;
 
-        /* Bottom right
-         * ix = Nx - 1, iy = 0 */
-        Lapl = (-2.0 * uE0 + u00 + ue0) * inv_dx_sq /* right */
-               + (-2.0 * uE0 + uE1 + uEE) * inv_dy_sq;  /* bottom */
-        grad = (u00 - ue0) * half_inv_dx /* right */
-               + (uE1 - uEE) * half_inv_dy; /* bottom */
+        // Bottom right
+        // ix = Nx - 1, iy = 0
+        Lapl = (-2.0 * uE0 + u00 + ue0) * inv_dx_sq // right
+               + (-2.0 * uE0 + uE1 + uEE) * inv_dy_sq;  // bottom
+        grad = (u00 - ue0) * half_inv_dx // right
+               + (uE1 - uEE) * half_inv_dy; // bottom
         u_new[Nx - 1][0] = uE0 - dt * uE0 * grad + dt * nu * Lapl;
 
-        /* Top left
-         * ix = 0, iy = Ny - 1 */
-        Lapl = (-2.0 * u0E + u1E + uEE) * inv_dx_sq /* left */
-               + (-2.0 * u0E + u00 + u0e) * inv_dy_sq; /* top */
-        grad = (u1E - uEE) * half_inv_dx /* left */
-               + (u00 - u0e) * half_inv_dx; /* top */
+        // Top left
+        // ix = 0, iy = Ny - 1
+        Lapl = (-2.0 * u0E + u1E + uEE) * inv_dx_sq // left
+               + (-2.0 * u0E + u00 + u0e) * inv_dy_sq; // top
+        grad = (u1E - uEE) * half_inv_dx // left
+               + (u00 - u0e) * half_inv_dx; // top
         u_new[0][Ny - 1] = u0E - dt * u0E * grad + dt * nu * Lapl;
 
-        /* Top right
-         * ix = Nx - 1, iy = Ny - 1 */
-        Lapl = (-2.0 * uEE + u0E + u[Nx - 1 - 1][Ny - 1]) * inv_dx_sq /* right */
-               + (-2.0 * uEE + uE0 + u[Nx - 1][Ny - 1 - 1]) * inv_dy_sq; /* top */
-        grad = (u0E - u[Nx - 1 - 1][Ny - 1]) * half_inv_dx + /* right */
-               +(uE0 - u[Nx - 1][Ny - 1 - 1]) * half_inv_dx; /* top */
+        // Top right
+        // ix = Nx - 1, iy = Ny - 1
+        Lapl = (-2.0 * uEE + u0E + u[Nx - 1 - 1][Ny - 1]) * inv_dx_sq // right
+               + (-2.0 * uEE + uE0 + u[Nx - 1][Ny - 1 - 1]) * inv_dy_sq; // top
+        grad = (u0E - u[Nx - 1 - 1][Ny - 1]) * half_inv_dx + // right
+               +(uE0 - u[Nx - 1][Ny - 1 - 1]) * half_inv_dx; // top
         u_new[Nx - 1][Ny - 1] = uEE - dt * uEE * grad + dt * nu * Lapl;
 
-        /* Edges */
-        /* Left and right
-         * ix = 0 and ix = Nx - 1 */
+        // Edges
+        // Left and right
+        // ix = 0 and ix = Nx - 1
         for (iy = 1; iy < Ny - 1; iy++) {
             double u0y = u[0][iy];
             double u1y = u[1][iy];
@@ -230,20 +229,20 @@ int main() {
             double uEyp = u[Nx - 1][iy + 1];
             double uEym = u[Nx - 1][iy - 1];
 
-            Lapl = (-2.0 * u0y + u1y + uEy) * inv_dx_sq  /* left */
+            Lapl = (-2.0 * u0y + u1y + uEy) * inv_dx_sq  // left
                    + (-2.0 * u0y + u0yp + u0ym) * inv_dy_sq;
             grad = (u1y - uEy) * half_inv_dx
                    + (u0yp - u0ym) * half_inv_dy;
             u_new[0][iy] = u0y - dt * u0y * grad + dt * nu * Lapl;
 
-            Lapl = (-2.0 * uEy + u0y + uey) * inv_dx_sq /* right */
+            Lapl = (-2.0 * uEy + u0y + uey) * inv_dx_sq // right
                    + (-2.0 * uEy + uEyp + uEym) * inv_dy_sq;
             grad = (u0y - uey) * half_inv_dx
                    + (uEyp - uEym) * half_inv_dy;
             u_new[Nx - 1][iy] = uEy - dt * uEy * grad + dt * nu * Lapl;
         }
-        /* Bottom and top
-         * iy = 0 and iy = Ny - 1 */
+        // Bottom and top
+        // iy = 0 and iy = Ny - 1
         for (ix = 1; ix < Nx - 1; ix++) {
             double ux0 = u[ix][0];
             double uxp0 = u[ix + 1][0];
@@ -255,19 +254,19 @@ int main() {
             double uxe = u[ix][Ny - 1 - 1];
 
             Lapl = (-2.0 * ux0 + uxp0 + uxm0) * inv_dx_sq
-                   + (-2.0 * ux0 + ux1 + uxE) * inv_dy_sq;  /* bottom */
+                   + (-2.0 * ux0 + ux1 + uxE) * inv_dy_sq; // bottom
             grad = (uxp0 - uxm0) * half_inv_dx
                    + (ux1 - uxE) * half_inv_dy;
             u_new[ix][0] = ux0 - dt * ux0 * grad + dt * nu * Lapl;
 
             Lapl = (-2.0 * uxE + uxpE + uxmE) * inv_dx_sq
-                   + (-2.0 * uxE + ux0 + uxe) * inv_dy_sq;     /* top */
+                   + (-2.0 * uxE + ux0 + uxe) * inv_dy_sq; // top
             grad = (uxpE - uxmE) * half_inv_dx
                    + (ux0 - uxe) * half_inv_dx;
             u_new[ix][Ny - 1] = uxE - dt * uxE * grad + dt * nu * Lapl;
         }
 
-        /* Interior */
+        // Interior
         for (ix = 1; ix < Nx - 1; ix++) {
             for (iy = 1; iy < Ny - 1; iy++) {
                 double uxy = u[ix][iy];
@@ -283,21 +282,17 @@ int main() {
                 Lapl += (-2.0 * uxy + uxpy + uxmy) * inv_dx_sq;
                 grad += (uxpy - uxmy) * half_inv_dx;
 
-                /* Compute d2u/dy2 */
-                Lapl += (-2.0 * uxy + uxyp + uxym) * inv_dy_sq;
-                grad += (uxyp - uxym) * half_inv_dy;
-
-                /* Compute new value of u at this grid point */
+                // Compute new value of u at this grid point
                 u_new[ix][iy] = uxy - dt * uxy * grad + dt * nu * Lapl;
             }
         }
 
-        /* Shunt u_new into u */
+        // Shunt u_new into u
         double** tmp = u_new;
         u_new = u;
         u = tmp;
 #ifdef NDEBUG
-        /* Snapshots of grid to file */
+        // Snapshots of grid to file
         if (istep == isnap) {
             sprintf(filename, "snapshot%08d.png", isnap);
             writePNG(filename, u, Nx, Ny);
@@ -306,18 +301,18 @@ int main() {
 #endif // NDEBUG
     }
 
-    /* END SECTION TO BE OPTIMISED */
+    // END SECTION TO BE OPTIMISED
 
-    /* Calculation time */
+    // Calculation time
     t2 = clock();
     printf("Time taken for %8d steps : %15.6f seconds\n", nstep, (double) (t2 - t1) / (double) CLOCKS_PER_SEC);
     fflush(stdout);
 
-    /* Write an image of the final grid */
+    // Write an image of the final grid
     sprintf(filename, "snapshot%08d.png", istep);
     writePNG(filename, u, Nx, Ny);
 
-    /* Write final time-evolved solution to file */
+    // Write final time-evolved solution to file
     FILE* fp = fopen("final_grid.dat", "w");
     if (fp == NULL) {
         fprintf(stderr, "Error opening final_grid.dat for output\n");
@@ -326,7 +321,7 @@ int main() {
 
     for (ix = 0; ix < Nx - 1; ix++) {
         for (iy = 0; iy < Ny - 1; iy++) {
-            /* x and y at the current grid points */
+            // x and y at the current grid points
             double x = dx * (double) ix;
             double y = dy * (double) iy;
             fprintf(fp, "%8.4f %8.4f %8.4e\n", x, y, u[ix][iy]);
@@ -335,7 +330,7 @@ int main() {
     }
     fclose(fp);
 
-    /* Release memory */
+    // Release memory
 #ifdef __OPTIMIZE__
     free2d(&u);
     free2d(&u_new);
