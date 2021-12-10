8154f5e5a69ff3ae26d45974cd1c65d05c923f19
Jlobblet <jlobblet-github@gmx.co.uk>
2021-12-10 04:06:45 +0000

    Fix send tags

--- Assignment4/comms.c
+++ Assignment4/comms.c
@@ -168,7 +168,7 @@ void comms_halo_swaps() {
         // Copy the left-hand elements into sendbuf
         sendbuf[iy] = grid_spin[iy][0];
     }
-    MPI_Bsend(sendbuf, grid_domain_size, MPI_INT, my_rank_neighbours[left], MPI_ANY_TAG, cart_comm);
+    MPI_Bsend(sendbuf, grid_domain_size, MPI_INT, my_rank_neighbours[left], 0, cart_comm);
     MPI_Recv(recvbuf, grid_domain_size, MPI_INT, my_rank_neighbours[right], MPI_ANY_TAG, cart_comm, &status);
     for (iy = 0; iy < grid_domain_size; iy++) {
         // Copy recvbuf into the right-hand elements
@@ -183,7 +183,7 @@ void comms_halo_swaps() {
         // Copy the right-hand elements into sendbuf
         sendbuf[iy] = grid_spin[iy][grid_domain_size - 1];
     }
-    MPI_Bsend(sendbuf, grid_domain_size, MPI_INT, my_rank_neighbours[right], MPI_ANY_TAG, cart_comm);
+    MPI_Bsend(sendbuf, grid_domain_size, MPI_INT, my_rank_neighbours[right], 0, cart_comm);
     MPI_Recv(recvbuf, grid_domain_size, MPI_INT, my_rank_neighbours[left], MPI_ANY_TAG, cart_comm, &status);
     for (iy = 0; iy < grid_domain_size; iy++) {
         // Copy recvbuf into the left-hand elements
@@ -196,7 +196,7 @@ void comms_halo_swaps() {
     // PRANCE FORWARD
     // Since the grid is stored this way around, can memcpy it.
     memcpy(sendbuf, grid_spin[0], grid_domain_size * sizeof(int));
-    MPI_Bsend(sendbuf, grid_domain_size, MPI_INT, my_rank_neighbours[down], MPI_ANY_TAG, cart_comm);
+    MPI_Bsend(sendbuf, grid_domain_size, MPI_INT, my_rank_neighbours[down], 0, cart_comm);
     MPI_Recv(recvbuf, grid_domain_size, MPI_INT, my_rank_neighbours[up], MPI_ANY_TAG, cart_comm, &status);
     memcpy(grid_spin[grid_domain_size - 1], recvbuf, grid_domain_size * sizeof(int));
 
@@ -205,7 +205,7 @@ void comms_halo_swaps() {
     // of grid halo. Remember to use the appropriate communicator.
     // BOOGIE DOWN
     memcpy(sendbuf, grid_spin[grid_domain_size - 1], grid_domain_size * sizeof(int));
-    MPI_Bsend(sendbuf, grid_domain_size, MPI_INT, my_rank_neighbours[up], MPI_ANY_TAG, cart_comm);
+    MPI_Bsend(sendbuf, grid_domain_size, MPI_INT, my_rank_neighbours[up], 0, cart_comm);
     MPI_Recv(recvbuf, grid_domain_size, MPI_INT, my_rank_neighbours[down], MPI_ANY_TAG, cart_comm, &status);
     memcpy(grid_spin[0], recvbuf, grid_domain_size * sizeof(int));
 
@@ -288,11 +288,11 @@ void comms_get_global_grid() {
     } else {
         // All other processors must send the data rank 0 needs
         // Send grid_domain_start to rank 0
-        MPI_Send(grid_domain_start, 2, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD);
+        MPI_Send(grid_domain_start, 2, MPI_INT, 0, 0, MPI_COMM_WORLD);
 
         // Loop over rows in the domain, sending them to rank 0
         for (iy = 0; iy < grid_domain_size; iy++) {
-            MPI_Send(combuff, grid_domain_size, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD);
+            MPI_Send(combuff, grid_domain_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
         }
     }
 
